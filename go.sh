9c1ffe26670a9ef8726229f9ccd72aac9d4197ab
Rob | Labs <roblabs@users.noreply.github.com>
2017-09-05 16:28:43 -0700

    More examples

---
+++ go.sh
@@ -0,0 +1,56 @@
+# load library
+. ./FS-vector2json.sh
+
+# EXAMPLES
+# vector2json S_USA.WildernessStatus.shp WILDERNESS 1241408010843 BWCAW
+# vector2json S_USA.WildernessStatus.shp WILDERNESS 1093403010667 Hauser
+# vector2json S_USA.WildernessStatus.shp WILDERNESS 1093404010667 Pine-Creek
+# vector2json S_USA.WildernessStatus.shp WILDERNESS 1052029010667 Golden-Trout
+# vector2json S_USA.WildernessStatus.shp WILDERNESS 1052028010667 John-Muir
+# vector2json S_USA.WildernessStatus.shp WILDERNESS 769879010667  Kachina-Peaks
+
+# Green Mountains
+vector2json S_USA.WildernessStatus.shp WILDERNESS 1178459010843 BRISTOL-CLIFFS-WILDERNESS
+vector2json S_USA.WildernessStatus.shp WILDERNESS 1606724010834 BREADLOAF-WILDERNESS
+vector2json S_USA.WildernessStatus.shp WILDERNESS 1606723010834 JOSEPH-BATTELL-WILDERNESS
+vector2json S_USA.RangerDistrict.shp RANGERDIST 99092001010343 Middlebury-Ranger-District
+vector2json S_USA.AdministrativeForest.shp ADMINFORES 99092000010343 Green-Mountain-and-Finger-Lakes-National-Forests
+
+#
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5092          SUCKER-BASSWOOD
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5094          SPLASH-NEWFOUND
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5095          FOUND-NEWFOUND
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5097          WIND-MOOSE
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5100          RIFLE-LAKE-LAKE-TWO
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5101          LAKE-ONE-LAKE-TWO-POND
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5102          LAKE-TWO-LAKE-ONE-POND
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5112          UNKNOWN
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5113          BASSWOOD-WIND
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5114          INDIANA-BASSWOOD
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5115          BASSWOOD-INDIANA
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5116          INDIANA-GOOD
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5118          HULA-WOOD
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5122          KAWISHIWI-RIVER-CONFUSION
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5123          LAKE-ONE-KAWISHIWI-RIVER
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5124          LAKE-ONE-CONFUSION
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5158          BASSWOOD-LAKE-HIDDEN-BAY
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5163          AZION-LAKE-BASSWOOD-LAKE
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5165          PIPESTONE-BAY-NEWTON-LAKE
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5166          FALL-LAKE-NEWTON-LAKE
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5182          FOURTOWN-FLOWAGE-FOURTOWN-LAKE
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5186          FOURTOWN-FLOWAGE-MUDRO-POND
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5187          MUDRO-POND-MUDRO-LAKE
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5188          SANDPIT-MUDDRO
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5194          PICKET-MUDRO
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5196          FALL-LAKE-NEWTON-LAKE
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5197          BASSWOOD-SUCKER
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5209          BASSWOOD-BACK-BAY
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5238          BASSWOOD-GOOD
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   5243          NEWFOUND-SKULL
+# WINTER
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   51228         HULA-WIND-BAY-WINTER
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   51196         JACKFISH-HORSE-WINTER
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   51197         FOUND-MANOMIN-WINTER
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   51198         MANOMIN-RICE-BAY-WINTER
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   51199         NEW-FOUND-SPLASH-WINTER
+# vector2json S_USA.TrailNFS_Publish.shp TRAIL_NO   51229         PIPESTONE-BASSWOOD-WINTER
